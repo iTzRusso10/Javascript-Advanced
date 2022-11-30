1401ec3365ca3a72573b97dc4f23c66036ec27d9
Giuseppe Russo <giusepperusso1003@gmail.com>
2022-11-30 20:54:51 +0100

    Conditional and Loops Exercise-1

---
+++ script.js
@@ -0,0 +1,34 @@
+function calculateSalary(role){
+ switch (role){
+    case "ceo":
+        salary = "The CEO salary is : 2200€"
+        break;
+    case "cto":
+        salary = "The CTO salary is : 1800€"
+        break;
+    case "manager":
+        salary = "The Manager salary is: 1800€"
+        break;
+    case "developer":
+        salary = "The Developer salary is : 1500€"
+        break;
+    case "other":
+        salary = "The default salary is :1000€"
+        break;
+ }
+ return salary;
+}
+
+
+const ceoSalary = calculateSalary("ceo");
+const managerSalary = calculateSalary("manager");
+const ctoSalary = calculateSalary("cto");
+const developerSalary = calculateSalary("developer");
+const otherSalary = calculateSalary("other");
+
+console.log(ceoSalary);
+console.log(managerSalary);
+console.log(ctoSalary);
+console.log(developerSalary);
+console.log(otherSalary);
+
