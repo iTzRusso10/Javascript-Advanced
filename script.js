b78a34bddd06193fb79a593de6cf09f31c74408e
Giuseppe Russo <giusepperusso1003@gmail.com>
2022-12-02 19:58:42 +0100

    Object Copying Exercise-5

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const person1 = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 25,
+};
+
+let person2 = Object.assign({}, person1);
+person2.firstName = "Simon"
+
+console.log(person1);
+console.log(person2);
+
