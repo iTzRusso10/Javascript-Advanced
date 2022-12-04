6c2a5b3e2f76aedbb6576ba130aea3467ae6c077
Giuseppe Russo <giusepperusso1003@gmail.com>
2022-12-04 04:08:40 +0100

    Closures-2 Exercise-7

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function printName() {
+  let helloName = "Hello John";
+  function inner() {
+    setTimeout(() => {
+        console.log(helloName);
+    }, 1000);
+    
+  }
+  return inner; 
+}
+
+printName()();
