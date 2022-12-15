1cd718ae32775ae4b82c88313f0bbccd35fd8479
Giuseppe Russo <giusepperusso1003@gmail.com>
2022-12-15 03:47:34 +0100

    Add comment

--- script.js
+++ script.js
@@ -6,3 +6,5 @@ function sum(...sum) {
 
 console.log(sum(1, 2, 3, 4, 5, 6, 7, 8, 9));
 
+// Il parametro ...sum prenderà un numero qualsiasi di elementi che andrà a sua volta a inserire in un array
+// Seguendo le regole degli array si fa il reduce
