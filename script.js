a69e9864ee3d0ae149b2549fbb507a5fa7e72ccb
Giuseppe Russo <giusepperusso1003@gmail.com>
2022-12-15 03:40:42 +0100

    Spread-Syntax Exercise-25

---
+++ script.js
@@ -0,0 +1,6 @@
+// Do you know how to "merge" the value of the newNumber variable into numberStore without using the array method push?
+
+let numberStore = [0, 1, 2];
+let newNumber = [...numberStore, 3]
+console.log(newNumber);
+
