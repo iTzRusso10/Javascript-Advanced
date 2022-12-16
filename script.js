b1f33b4b825bba2e48a1e7d033a4fbc306f8b10a
Giuseppe Russo <giusepperusso1003@gmail.com>
2022-12-16 15:58:07 +0100

    JSON-Methods Exercise-30

---
+++ script.js
@@ -0,0 +1,26 @@
+// Create the fromJson method that takes in a json as parameter and returns an object of type Person.
+
+class Person {
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  toJson() {
+    return JSON.stringify(this);
+  }
+
+  static fromJson(val) {
+    let obj = JSON.parse(val);
+    return new Person (obj.id, obj.firstName, obj.lastName, obj.age)
+}
+}
+
+
+
+const json = '{"id":1,"firstName":"Mario","lastName":"Rossi","age":25}';
+const developer = Person.fromJson(json);
+console.log(developer);
+
