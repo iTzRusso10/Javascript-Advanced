dd7f96614d49c2ff406011e85756e5d603c3fb7b
Giuseppe Russo <giusepperusso1003@gmail.com>
2022-12-16 15:36:13 +0100

    JSON-Methods Exercise-29

---
+++ script.js
@@ -0,0 +1,15 @@
+// Convert the developer object into JSON
+
+class Person {
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+}
+
+const developer = new Person(1, 'Mario', 'Rossi', 25);
+
+let json= JSON.stringify(developer);
+console.log(json)
